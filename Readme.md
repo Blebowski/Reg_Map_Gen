478410814b1234f1b5a24cabebf567663ee89421
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2018-11-28 22:26:25 +0100

    Added Readme and changed license header.

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# IP-XACT Register map generation tool
+
+Generator of register map design sources and documentation from IP-XACT format.
+
+Following artifact are generated:
+    - Lyx documentation
+    - VHDL package with constants definition for testbench or design
+    - C Header File
+    - Synthesizable VHDL RTL implementation of register map.
+
--- gen_lib.py
+++ gen_lib.py
@@ -1,10 +1,32 @@
 ################################################################################                                                     
 ## 
-##   CAN with Flexible Data-Rate IP Core 
-##   
-##   Copyright (C) 2018 Ondrej Ille <ondrej.ille@gmail.com>
-##   
-##	 Library with auxiliarly functions for pyXact generator.   
+## Register map generation tool
+##
+## Copyright (C) 2018 Ondrej Ille <ondrej.ille@gmail.com>
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this SW component and associated documentation files (the "Component"),
+## to deal in the Component without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Component, and to permit persons to whom the
+## Component is furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Component.
+##
+## THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHTHOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
+## IN THE COMPONENT.
+##
+###############################################################################
+
+###############################################################################	 
+## 
+## Library with auxiliarly functions for pyXact generator.   
 ##
 ##	Revision history:
 ##		24.01.2018	First implementation based on the previous stand-alone
